0c7591ee97ae7bf3881fe6d8932c3e8bcbbd4546
Sudharshan S <79196955+sudharshans2009@users.noreply.github.com>
2024-03-08 21:04:01 +0530

    Update ProfileCard.tsx

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -14,7 +14,7 @@ const ProfileCard: React.FC<Props> = () => {
       </div>
       <div className="content">
         <div className="top">
-          <Image src={CONFIG.profile.image} fill alt="" />
+          <Image src={CONFIG.profile.image} fill alt="" style={{ borderRadius: "10px" }} />
         </div>
         <div className="mid">
           <div className=" name">{CONFIG.profile.name}</div>
