52e3227e7e1987317033872d43fc326fe077a043
Sudharshan S <79196955+sudharshans2009@users.noreply.github.com>
2024-03-08 21:10:16 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,11 @@ class MyDocument extends Document {
     return (
       <Html lang={CONFIG.lang}>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/madlad.jpg" />
           <link
             rel="apple-touch-icon"
             sizes="192x192"
-            href="/apple-touch-icon.png"
+            href="/madlad.jpg"
           ></link>
           <link
             rel="alternate"
