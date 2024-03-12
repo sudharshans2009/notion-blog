0cdd7b8494bc085e1c3fade66f437af55966d9bc
Sudharshan S <79196955+sudharshans2009@users.noreply.github.com>
2024-03-12 04:00:39 +0000

    Update Vercel v3.2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,16 +3,6 @@ import { CONFIG } from "site.config"
 
 class MyDocument extends Document {
   render() {
-    window.addEventListener("load", async () => {
-      if ("serviceWorker" in navigator) {
-        try {
-          await navigator.serviceWorker.register("/worker.js");
-        } catch (e) {
-          console.log("SW registration failed");
-        }
-      }
-    });
-    
     return (
       <Html lang={CONFIG.lang}>
         <Head>
