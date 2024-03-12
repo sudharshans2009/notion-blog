81212aa5d92b0303fcc3b09b409aaede39d1206d
Sudharshan S <79196955+sudharshans2009@users.noreply.github.com>
2024-03-12 05:41:35 +0000

    Update Vercel v3.5

--- site.config.js
+++ site.config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     email: "martin.nicolai.online@gmail.com",
     linkedin: "martinwriter1",
     github: "",
-    instagram: "",
+    instagram: "martinwriter_",
     twitter: "notionmadlad"
   },
   projects: [
@@ -52,7 +52,7 @@ const CONFIG = {
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
-      label: "💬 Utterances",
+      label: "Utterances",
     },
   },
   cusdis: {
